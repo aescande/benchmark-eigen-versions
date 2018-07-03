377747fafe8dad42aea772ee934d4f147acd5192
Adrien Escande <adrien.escande@gmail.com>
2018-07-03 15:05:46 +0900

    Householder multiplication test

--- bench.in.cpp
+++ bench.in.cpp
@@ -2,67 +2,81 @@
 
 #include "Eigen/Core"
 
+typedef Eigen::Matrix<double,1,Eigen::Dynamic> RowVector;
+
 static void BM_@NAME@_NormalProduct(benchmark::State & state)
 {
-  Eigen::MatrixXd T = Eigen::MatrixXd::Random(30, 40);
+  Eigen::MatrixXd M = Eigen::MatrixXd::Random(30, 40);
+  RowVector m = RowVector::Random(40);
   Eigen::VectorXd u = Eigen::VectorXd::Random(30);
-  Eigen::Matrix<double,1,Eigen::Dynamic> v = Eigen::Matrix<double, 1, Eigen::Dynamic>::Random(40);
-  double t = 0.3;
-
-  double d = 0;
+  RowVector v0 = RowVector::Random(40);
+  RowVector v(40);
+  double tau = 0.3;
+  
   for(auto _ : state)
   {
-    T.noalias() -= t * u * v;
-    d = T.data()[0];
+    v = v0;
+    v += m;
+    m -= tau*v;
+    M.noalias() -= tau * u * v;
   }
 }
 BENCHMARK(BM_@NAME@_NormalProduct);
 
 static void BM_@NAME@_LazyProduct(benchmark::State & state)
 {
-  Eigen::MatrixXd T = Eigen::MatrixXd::Random(30, 40);
+  Eigen::MatrixXd M = Eigen::MatrixXd::Random(30, 40);
+  RowVector m = RowVector::Random(40);
   Eigen::VectorXd u = Eigen::VectorXd::Random(30);
-  Eigen::Matrix<double,1,Eigen::Dynamic> v = Eigen::Matrix<double, 1, Eigen::Dynamic>::Random(40);
-  double t = 0.3;
+  RowVector v0 = RowVector::Random(40);
+  RowVector v(40);
+  double tau = 0.3;
 
-  double d = 0;
   for(auto _ : state)
   {
-    T.noalias() -= t * u.lazyProduct(v);
-    d = T.data()[0];
+    v = v0;
+    v += m;
+    m -= tau*v;
+    M.noalias() -= tau * u.lazyProduct(v);
   }
 }
 BENCHMARK(BM_@NAME@_LazyProduct);
 
 static void BM_@NAME@_GroupProduct(benchmark::State & state)
 {
-  Eigen::MatrixXd T = Eigen::MatrixXd::Random(30, 40);
+  Eigen::MatrixXd M = Eigen::MatrixXd::Random(30, 40);
+  RowVector m = RowVector::Random(40);
   Eigen::VectorXd u = Eigen::VectorXd::Random(30);
-  Eigen::Matrix<double,1,Eigen::Dynamic> v = Eigen::Matrix<double, 1, Eigen::Dynamic>::Random(40);
-  double t = 0.3;
+  RowVector v0 = RowVector::Random(40);
+  RowVector v(40);
+  double tau = 0.3;
 
-  double d = 0;
   for(auto _ : state)
   {
-    T.noalias() -= (t*u) * v;
-    d = T.data()[0];
+    v = v0;
+    v += m;
+    m -= tau*v;
+    M.noalias() -= (tau*u) * v;
   }
 }
 BENCHMARK(BM_@NAME@_GroupProduct);
 
 static void BM_@NAME@_SeparatedProduct(benchmark::State & state)
 {
-  Eigen::MatrixXd T = Eigen::MatrixXd::Random(30, 40);
+  Eigen::MatrixXd M = Eigen::MatrixXd::Random(30, 40);
+  RowVector m = RowVector::Random(40);
   Eigen::VectorXd u = Eigen::VectorXd::Random(30);
-  Eigen::Matrix<double,1,Eigen::Dynamic> v = Eigen::Matrix<double, 1, Eigen::Dynamic>::Random(40);
-  double t = 0.3;
+  RowVector v0 = RowVector::Random(40);
+  RowVector v(40);
+  double tau = 0.3;
 
-  double d = 0;
   for(auto _ : state)
   {
-    v *= -t;
-    T.noalias() += u * v;
-    d = T.data()[0];
+    v = v0;
+    v += m;
+    v *= -tau;
+    m += v;
+    M.noalias() += u * v;
   }
 }
 BENCHMARK(BM_@NAME@_SeparatedProduct);
