05dc5e3395ee98fab84e9a2d8fd5c4611544c521
Adrien Escande <adrien.escande@gmail.com>
2018-07-03 14:24:41 +0900

    add variation

--- bench.in.cpp
+++ bench.in.cpp
@@ -50,4 +50,21 @@ static void BM_@NAME@_GroupProduct(benchmark::State & state)
 }
 BENCHMARK(BM_@NAME@_GroupProduct);
 
+static void BM_@NAME@_SeparatedProduct(benchmark::State & state)
+{
+  Eigen::MatrixXd T = Eigen::MatrixXd::Random(30, 40);
+  Eigen::VectorXd u = Eigen::VectorXd::Random(30);
+  Eigen::Matrix<double,1,Eigen::Dynamic> v = Eigen::Matrix<double, 1, Eigen::Dynamic>::Random(40);
+  double t = 0.3;
+
+  double d = 0;
+  for(auto _ : state)
+  {
+    v *= -t;
+    T.noalias() += u * v;
+    d = T.data()[0];
+  }
+}
+BENCHMARK(BM_@NAME@_SeperatedProduct);
+
 BENCHMARK_MAIN()
